ca52c823220737e85b3ef1919b0a187e7c453202
Joachim Birkelund Fredheim <joachimbf@uia.no>
2020-09-25 15:59:11 +0200

    Fikset Ex grafing

--- del1/oppgave2/1d1.py
+++ del1/oppgave2/1d1.py
@@ -8,37 +8,42 @@ max = 2
 def f(x):
     return (14*x) - 8
 
-def g(x):
-    return np.longdouble((F(x0+x)-F(x0))/x)
+def g(dx,x=x0):
+    return np.longdouble((F(x+dx)-F(x))/dx)
 
 def G(x):
     return 0.5+np.longdouble((F(x+0.00014)-F(x))/0.00014)
-    #+0.5 for å flytte grafen litt opp fra f'
 
 def F(x):
     return 7*x**2 -8*x +1
 
 
 for x in np.arange(0,0.001,0.000001):
-    Ex = abs(f(x0)-g(x)) if abs(f(x0)-g(x))<=0.001 else abs(f(x0)-g(x-0.000001))
+    Ex = abs(f(x0)-g(dx=x))
     if(Ex<=0.001):
-        print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
+        valid=x
+        #print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
     elif(Ex>0.001):
         break
 
-print(Ex)
+i=0
+E = [0]*1000
+for x in np.linspace(min,max,1000):
+    E[i] = abs(f(x)-g(x=x,dx=valid))
+    i+=1
+
+
 fig = plt.gcf()
 fig.canvas.set_window_title('Funksjon 1: 7x² - 8x + 1')
 x=np.linspace(min,max,1000)
 y=F(x)
 yd=f(x)
 yg=G(x)
-ye=[]
-ye=np.array(np.full(1000,Ex))
+ye=E
 plt.grid(True)
 plt.plot(x,yd,'r-', label='f \'(x)')
-plt.plot(x,y,'g-',label="f (x)")
+plt.plot(x,y,'g-',label="F (x)")
 plt.plot(x,yg,'b-',label="g (x) + 0.5")
-plt.plot(x,ye,color='purple',label='E (x)')
+plt.plot(x,ye,color='purple',label='E (x): for deltaX %.5f'%(valid))
 plt.legend()    
 plt.show()
--- del1/oppgave2/1d4.py
+++ del1/oppgave2/1d4.py
@@ -8,8 +8,11 @@ max = 10
 def f(x):
     return x/np.sqrt(1+x**2)
 
-def g(x):
-    return np.longdouble((F(x0+x)-F(x0))/x)
+def g(dx,x=x0):
+    return np.longdouble((F(x+dx)-F(x))/dx)
+
+def G(x):
+    return 0.5+np.longdouble((F(x+0.28)-F(x))/0.28)
 
 def F(x):
     return np.sqrt(1+x**2)
@@ -18,21 +21,29 @@ def F(x):
 for x in np.arange(0.275,max-x0,0.000001):
     Ex = abs(f(x0)-g(x))
     if(Ex<=0.001):
-        print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
+        valid=x
+        #print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
     elif(Ex>0.001):
         break
+
+i=0
+E = [0]*1000
+for x in np.linspace(min,max,1000):
+    E[i] = 5*np.longdouble(abs(f(x)-g(x=x,dx=valid)))
+    i+=1
+
 
 fig = plt.gcf()
 fig.canvas.set_window_title('Funksjon 4: sqrt(1+x²)')
 x=np.linspace(min,max,1000)
 y=F(x)
 yd=f(x)
-yg=g(x)
-ye=abs(f(x0)-g(x))
+yg=G(x)
+ye=E
 plt.grid(True)
 plt.plot(x,yd,'r-', label='f \'(x)')
-plt.plot(x,y,'g-',label="f (x)")
+plt.plot(x,y,'g-',label="F (x)")
 plt.plot(x,yg,'b-',label="g (x)")
-plt.plot(x,ye,color='purple',label='E (x)')
+plt.plot(x,ye,color='purple',label='5*E (x) for deltaX %.2f'%(valid))
 plt.legend()    
 plt.show()
