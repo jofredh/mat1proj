baa11a461c398d3b035811dd011a4bc29ba7815a
Tobias Brambo <tobiasmb@uia.no>
2020-09-25 13:46:05 +0200

    Gjort oppdatering

---
+++ del2/newtonRaphson_oppg2b.py
@@ -0,0 +1,58 @@
+'''
+Denne koden tregner 2 input fra brukeren. Den trenger en start verdi på hvor
+man tror det ene nullpunktet er. Og det trenger en mengde iterasjoner den
+maksimalt skal gjenta seg, før den sier at det ikke konvergerer.
+Hvis start verdien er nærme nok til at det kommer innenfor feilmarginen
+før antall iterasjoner er nådd vil den si hvor nullpunktet er.
+'''
+# HUSK AT DETTE GJELDER FOR BEGGE NULLPUNKTENE +2*PI*K
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+x0 = input("Start verdi: ")
+e = 10**(-12)
+N = input("Max iterasjoner: ")
+
+x0 = float(x0)
+e = float(e)
+N = int(N)
+x = x0
+cos = math.cos
+sin = math.sin
+
+def f(x):
+    return (10 - (8.5*cos(x)))**2 + (6 - (2 + 8.5*sin(x)))**2 - 49
+
+def g(x):
+    return -68*cos(x) + 170*sin(x)
+
+def newtonRaphson(x0,e,N):
+    print('\nNewton Raphson metoden:')
+    step = 1
+    flag = 1
+    condition = True
+
+    while condition:
+        if(g(x0) == 0.0):
+            print("kan ikke dele på 0")
+            break
+        
+        x1 = x0 - f(x0)/g(x0)
+        print('Iterasjon-%d, x1 = %0.12f og f(x1) = %0.12f' % (step, x1, f(x1)))
+        x0 = x1
+        step= step + 1
+
+        if step > N:
+            flag = 0
+            break
+
+        condition= abs(f(x1)) > e
+
+    if flag == 1:
+        print('\nDet ene nullpunktet er i: %0.12f' % x1)
+    else:
+        print("\nIkke konvergent.")
+
+newtonRaphson(x0,e,N)
