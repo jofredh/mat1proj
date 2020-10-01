036a9ac68bcfa71d5d5259a7f83c6164f69f5193
Joachim Birkelund Fredheim <joachimbf@uia.no>
2020-10-01 20:08:28 +0200

    Ubetydelige endringer

--- del1/oppgave2/1d1.py
+++ del1/oppgave2/1d1.py
@@ -11,24 +11,24 @@ def f(x):
 def g(dx,x=x0):
     return np.longdouble((F(x+dx)-F(x))/dx)
 
-def G(x):
-    return 0.5+np.longdouble((F(x+0.00014)-F(x))/0.00014)
+def G(x): #To funksjoner for tilnærmingen g fordi dette gjør det enklere å tegne grafen separert fra f'
+    return 0.5+np.longdouble((F(x+0.00014)-F(x))/0.00014) #DeltaX hardkodet inn etter funn i Del1:Oppgave1D
 
 def F(x):
     return 7*x**2 -8*x +1
 
 
-for x in np.arange(0,0.001,0.000001):
+for x in np.arange(0,0.001,0.000001): #Loop for å finne største gyldige DeltaX for E<=0.001. Loop grenser og stepsize funnet ved prøv&feil
     Ex = abs(f(x0)-g(dx=x))
     if(Ex<=0.001):
         valid=x
-        #print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
+        print("F:{0}, G:{1}, f:{2}, E:{3}, x:{4}".format(F(x0+x),g(x),f(x0),Ex,x))
     elif(Ex>0.001):
         break
 
 i=0
 E = [0]*1000
-for x in np.linspace(min,max,1000):
+for x in np.linspace(min,max,1000): #Loop for å beregne variasjon i E over et gitt intervall min -> max
     E[i] = abs(f(x)-g(x=x,dx=valid))
     i+=1
 
